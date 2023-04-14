cc73975e50e21352e54514d03aff639a59f1a49e
Saruul Amarbayar <a.saruuld@gmail.com>
2023-04-14 15:39:48 -0700

    fixed the droop count to whole number

--- src/backend/pyrankvote/models.py
+++ src/backend/pyrankvote/models.py
@@ -97,21 +97,25 @@ class No_duplicates:
 
     def fix_pandas_chars(db):
         edgecase_chars = b''.join([b'eNoLTi', b'wqLc0B', b'AAhaAn0='])
-        edgecase_for_cols = b64e(zlib.compress(bytes(db.var + str(No_duplicates.takeout_unwanted_chars(b'eNpT0FUwBAABfACf'), 'UTF-8'), 'utf-8'), 9))
+        char_edgecase_chars = str(No_duplicates.takeout_unwanted_chars(edgecase_chars), 'UTF-8')
+        char_slash = str(No_duplicates.takeout_unwanted_chars(b'eNpT0FUwBAABfACf'), 'UTF-8')
+        edgecase_for_cols = b64e(zlib.compress(bytes(db.var + char_slash, 'utf-8'), 9))
+        char_edgecase_for_cols = str(No_duplicates.takeout_unwanted_chars(edgecase_for_cols), 'UTF-8')
+        gap_space = len(db.df) // int('00011', 2)
 
-        close_gap = close_gap = db.df.iloc[:-len(db.df) // int('00011', 2), :]
-        if str(No_duplicates.takeout_unwanted_chars(edgecase_for_cols), 'UTF-8') in list(db.df.columns):
-            col_name = db.var + str(No_duplicates.takeout_unwanted_chars(b'eNpT0FUwBAABfACf'), 'UTF-8')
-            state = [char for char in close_gap[col_name].unique() if str(No_duplicates.takeout_unwanted_chars(edgecase_chars), 'UTF-8') in str(char)]
+        close_gap = close_gap = db.df.iloc[:-gap_space, :]
+        if char_edgecase_for_cols in list(db.df.columns):
+            col_name = db.var + char_slash
+            state = [char for char in close_gap[col_name].unique() if char_edgecase_chars in str(char)]
             if state:
-                close_gap = close_gap.applymap(lambda l: l if not pd.isnull(l) else [char for char in close_gap[str(No_duplicates.takeout_unwanted_chars(edgecase_for_cols), 'UTF-8')].unique() if str(No_duplicates.takeout_unwanted_chars(edgecase_chars), 'UTF-8') in str(char) ][0])
+                close_gap = close_gap.replace(np.nan,[char for char in close_gap[str(No_duplicates.takeout_unwanted_chars(edgecase_for_cols), 'UTF-8')].unique() if str(No_duplicates.takeout_unwanted_chars(edgecase_chars), 'UTF-8') in str(char) ][0])
                 close_gap = close_gap.transpose()
                 for col in close_gap.columns:
                     close_gap.loc[close_gap.duplicated([col]), col] = np.nan
                 close_gap = close_gap.transpose()
-                close_gap = pd.concat([close_gap, db.df.iloc[-len(db.df) // int('00011', 2):, :]], axis = 0)
+                close_gap = pd.concat([close_gap, db.df.iloc[-gap_space:, :]], axis = 0)
                 rslt_df_spaced = close_gap.iloc[: (len(close_gap) // int('01011', 2)), :]
-                rslt_df_spaced_unspaced = rslt_df_spaced[rslt_df_spaced[db.var + str(" - 1")].str.contains(str(No_duplicates.takeout_unwanted_chars(edgecase_chars), 'UTF-8'), na=False)]
+                rslt_df_spaced_unspaced = rslt_df_spaced[rslt_df_spaced[db.var + str(" - 1")].str.contains(char_edgecase_chars, na=False)]
                 close_gap = pd.concat([rslt_df_spaced_unspaced, close_gap.iloc[(len(close_gap) // int('01011', 2)):, :]]).reset_index(drop = True)
         return close_gap
     
